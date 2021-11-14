4c13a9a921bd8137451e8b0e7e572a8e69ca9dc1
John Garcia <johnkegd@gmail.com>
2021-11-14 21:08:08 +0100

    mesh groups, lights, axesHelper, Clock, animationFrame, position sin and cos, lookAt

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { Group } from 'three';
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -7,13 +8,35 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
+
 /**
- * Objects
+ * Objects group
+ * 
+*/
+const geometriesGroup = new THREE.Group();
+
+
+/**
+ * Objects boxMesh
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const boxMesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }));
+boxMesh.scale.z = 2;
+boxMesh.rotation.y = Math.PI / 1.25;
+geometriesGroup.add(boxMesh);
+
+/**
+ * Objects sphereMesh
+ */
+const sphereMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshBasicMaterial({ color: 0xffb606, wireframe: true }));
+sphereMesh.position.x = 6;
+geometriesGroup.add(sphereMesh)
+
+geometriesGroup.position.x = 1;
+scene.add(geometriesGroup);
 
 /**
  * Sizes
@@ -26,15 +49,54 @@ const sizes = {
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.x = 0.3;
+camera.position.y = 2;
+camera.position.z = 10;
+camera.lookAt(boxMesh.position);
 scene.add(camera);
+
+/**
+ * Lights
+ */
+const light = new THREE.Light(0xffffff, 2.0);
+light.position.x = 2;
+scene.add(light);
+
+
+/**
+ * axesHelper
+ */
+const axesHelper = new THREE.AxesHelper(8);
+scene.add(axesHelper);
+
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+
+// Clock
+const clock = new THREE.Clock();
+
+
+function animation() {
+    requestAnimationFrame(animation);
+    const elapsedTime = clock.getElapsedTime();
+    boxMesh.position.x = Math.cos(elapsedTime);
+    boxMesh.rotation.y = elapsedTime * Math.PI * 1;
+    sphereMesh.rotation.x = elapsedTime * Math.PI * 2;
+    sphereMesh.position.y = Math.sin(elapsedTime + 3.10);
+    sphereMesh.position.x = Math.cos(elapsedTime);
+
+    camera.lookAt(sphereMesh.position);
+    renderer.render(scene, camera);
+}
+
+animation();
