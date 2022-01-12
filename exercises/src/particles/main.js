d12a6bf68b7f6a2ea953e9bb237d128e0e9d339a
John Garcia <johnkegd@gmail.com>
2022-01-12 23:34:40 +0100

    adding particles transparency and alpha via depthWrite

--- exercises/src/particles/main.js
+++ exercises/src/particles/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import * as dat from 'lil-gui';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import resizeCanvas from '../utils/resizeCanvas';
 import particles from './particle';
 
@@ -19,16 +19,22 @@ console.log(particles.geometry);
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.heigth);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
+//renderer.setClearColor('#323232');
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.heigth);
 camera.position.z = 5;
-const orbitControls = new OrbitControls(camera,canvas);
+const orbitControls = new OrbitControls(camera, canvas);
 orbitControls.enableDamping = true;
 
 
 const axesHelper = new THREE.AxesHelper(8);
 scene.add(axesHelper);
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshBasicMaterial()
+);
+scene.add(cube);
 
 scene.add(camera);
 particles.geometry.center();
--- exercises/src/particles/particle.js
+++ exercises/src/particles/particle.js
@@ -1,21 +1,35 @@
 import * as THREE from 'three';
 
 const particlesGeometry = new THREE.BufferGeometry();
-const count = 500;
+const count = 1500;
+
+const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load('/textures/particles/2.png');
 
 const positions = new Float32Array(count * 3);
 
-for(let i = 0; i< count; i++) {
+for (let i = 0; i < count; i++) {
     positions[i] = (Math.random() - 0.5) * 10;
 }
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions,3));
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
 
 const particlesMaterial = new THREE.PointsMaterial({
     size: 0.02,
     sizeAttenuation: true
 });
+
+//particlesMaterial.map = particleTexture;
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture
 particlesMaterial.size = 0.1;
+
+particlesMaterial.color = new THREE.Color('#ffb606');
+//particlesMaterial.alphaTest = 0.001;
+//particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+
+
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 
 
