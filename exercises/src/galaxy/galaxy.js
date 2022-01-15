898ce7ec0e177031c5bde398ffc1a33bc070769a
John Garcia <johnkegd@gmail.com>
2022-01-15 20:27:10 +0100

    ignoring exercises/dist, changed page title, adding randomness to galaxy gen

--- exercises/src/galaxy/galaxy.js
+++ exercises/src/galaxy/galaxy.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-const parameters = { count: 1000, size: 0.02, radius: 5, branches: 3, spin: 1 };
+const parameters = { count: 1000, size: 0.02, radius: 5, branches: 3, spin: 1, randomness: 0.2 };
 let selfGui = null;
 let selfScene = null;
 
@@ -12,8 +12,6 @@ const material = new THREE.PointsMaterial({
     blending: THREE.AdditiveBlending
 });
 let points = null;
-
-
 
 
 function galaxyBigBang(scene, gui) {
@@ -37,12 +35,16 @@ function galaxyBigBang(scene, gui) {
         const i3 = i * 3;
 
         const radius = Math.random() * parameters.radius;
-        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
         const spinAngle = radius * parameters.spin;
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
 
-        positions[i3] = Math.cos(branchAngle + spinAngle) * radius;
-        positions[i3 + 1] = 0;
-        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
+        const randomY = (Math.random() - 0.5) * parameters.randomness * radius;
+        const randomX = (Math.random() - 0.5) * parameters.randomness * radius;
+        const randomZ = (Math.random() - 0.5) * parameters.randomness * radius;
+
+        positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+        positions[i3 + 1] = randomY;
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
 
     }
     points = new THREE.Points(geometry, material);
@@ -60,6 +62,8 @@ function setGui() {
         selfGui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(galaxyBigBang);
         selfGui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(galaxyBigBang)
         selfGui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(galaxyBigBang);
+        selfGui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(galaxyBigBang);
+        selfGui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(galaxyBigBang);
     }
 }
 
